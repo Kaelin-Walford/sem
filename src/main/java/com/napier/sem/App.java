9a7d9d183e7af8f2c2ea40a34ab2058e49d6c21f
Kaelin-Walford <40650381@live.napier.ac.uk>
2024-10-11 15:58:18 +0100

    added more to app.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
@@ -11,10 +12,12 @@ public class App
 
         // Connect to database
         a.connect();
-        // Get Employee
-        Employee emp = a.getEmployee(255530);
-        // Display results
-        a.displayEmployee(emp);
+
+        // Extract employee salary information
+        ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
 
         // Disconnect from database
         a.disconnect();
@@ -91,12 +94,17 @@ public class App
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
-            String strSelect =
-                    "SELECT employees.emp_no AS emp_no_employee, employees.first_name, employees.last_name, titles.title, salaries.salary "
-                            + "FROM employees JOIN titles ON employees.emp_no = titles.emp_no "
-                            + "JOIN salaries ON employees.emp_no = salaries.emp_no "
-                            + "WHERE emp_no = " + ID;
+            //String strSelect =
+                    //"SELECT employees.emp_no AS emp_no_employee, employees.first_name, employees.last_name, titles.title, salaries.salary "
+                            //+ "FROM employees JOIN titles ON employees.emp_no = titles.emp_no "
+                            //+ "JOIN salaries ON employees.emp_no = salaries.emp_no "
+                            //+ "WHERE emp_no = " + ID;
             // Execute SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary" +
+                            "FROM employees, salaries" +
+                            "WHERE employees.emp_no = salaries.emp_no AND salaries.emp_no = '9999-01-01'" +
+                            "ORDER BY employees.emp_no ASC";
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
@@ -133,6 +141,44 @@ public class App
                             + "Salarys:" + emp.salary + "\n"
                             + emp.dept_name + "\n"
                             + "Manager: " + emp.manager + "\n");
+        }
+    }
+    /**
+     * Gets all the current employees and salaries.
+     * @return A list of all employees and salaries, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalaries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                            + "FROM employees, salaries "
+                            + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
+                            + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
         }
     }
 }
